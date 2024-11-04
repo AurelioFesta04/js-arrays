a8258d092042c503b4844b60ad2583c09a6e5603
AurelioFesta04 <auryfesta@gmail.com>
2024-11-04 10:31:10 +0100

    refactoring: adjust values

--- js-arrays/01-arrays/script.js
+++ js-arrays/01-arrays/script.js
@@ -57,5 +57,5 @@ console.log(teachersString);
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = teachers.splice();
+const isTeachersEmpty = teachers.length === 0;
 console.log(isTeachersEmpty);
